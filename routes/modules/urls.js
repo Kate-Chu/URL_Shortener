9e9bfc254b28e2b99f0754216506eb3b9656a77f
KateChu <chu.shihchien@gmail.com>
2022-03-25 10:09:56 +0800

    adjust 資料庫查詢, 未來待修： string-generator.js // Object.keys

--- routes/modules/urls.js
+++ routes/modules/urls.js
@@ -5,44 +5,35 @@ const randomString = require("../../string-generator");
 
 router.get("/result", (req, res) => {
   const suburl = req.query.suburl;
-  Url.find()
-    .lean()
-    .then((urls) => {
-      const existUrl = urls.filter(
-        (url) => url.originalUrl.toLowerCase() === suburl.toLowerCase()
-      );
 
-      if (existUrl.length) {
-        const shortUrl = existUrl[0].shortUrl;
-        const originalUrl = existUrl[0].originalUrl;
-        res.render("result", { suburl, originalUrl, shortUrl });
-      } else {
-        let urlRandStr = randomString(5);
-        let shortUrl = req.headers.host + "/urls/" + urlRandStr;
-        const url = new Url({
-          originalUrl: suburl,
-          shortUrl,
-          urlRandStr,
-        });
-        return url
-          .save()
-          .then(() => {
-            res.render("result", {
-              suburl,
-              shortUrl,
-              originalUrl: suburl,
-            });
-          })
-          .catch((error) => {
-            console.log(error);
-            res.render("error", { error });
+  Url.findOne({ originalUrl: suburl }).then((existUrl) => {
+    if (existUrl) {
+      const shortUrl = existUrl.shortUrl;
+      const originalUrl = existUrl.originalUrl;
+      res.render("result", { suburl, originalUrl, shortUrl });
+    } else {
+      let urlRandStr = randomString(5);
+      let shortUrl = req.headers.host + "/urls/" + urlRandStr;
+      const url = new Url({
+        originalUrl: suburl,
+        shortUrl,
+        urlRandStr,
+      });
+      return url
+        .save()
+        .then(() => {
+          res.render("result", {
+            suburl,
+            shortUrl,
+            originalUrl: suburl,
           });
-      }
-    })
-    .catch((error) => {
-      console.log(error);
-      res.render("error", { error });
-    });
+        })
+        .catch((error) => {
+          console.log(error);
+          res.render("error", { error });
+        });
+    }
+  });
 });
 
 router.get("/:urlRandStr", (req, res) => {
